ca09d236305ff019ffa52e7e8ce5c741ea53cbd4
GofyGarado <geoffgarado@gmail.com>
2025-01-25 21:00:56 +0800

    Current neovim files
    Debloated craftzdog neovim setup

---
+++ lua/chadrc.lua
@@ -0,0 +1,28 @@
+local M = {}
+
+M.base46 = {
+	theme = "catppuccin",
+	transparency = true,
+}
+
+M.lsp = {
+	signature = false,
+}
+
+M.nvdash = {
+	load_on_startup = true,
+
+	header = {
+		"                            ",
+		"                            ",
+		"|--------------------------|",
+		"|    Geoff Gerad Garado    |",
+		"|--------------------------|",
+		"        \\ (•◡•) //         ",
+		"         \\     //          ",
+		"                            ",
+		"                            ",
+	},
+}
+
+return M
---
+++ lua/options.lua
@@ -0,0 +1,7 @@
+require("nvchad.options")
+
+local opt = vim.opt
+
+opt.relativenumber = true
+opt.scrolloff = 5
+opt.guifont = "FiraCode Nerd Font:h12"
